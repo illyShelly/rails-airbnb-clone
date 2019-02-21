ed5c21f93add825319ebedf4478ffe2b23a4407a
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-21 22:17:29 +0100

    config delete just passed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # ADDED
-  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
-
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
