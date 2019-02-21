291b1ea32c4f2ef237f2ff357a83708ecb70cfaa
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-21 22:23:00 +0100

    typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bookings, dependent: :destroy
-  has_many :flats, dependent: :dependent :destroy
+  has_many :flats, dependent: :destroy
 end
