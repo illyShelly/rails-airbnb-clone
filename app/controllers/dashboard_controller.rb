c29fad375d186f4059a2b0ee4315e81e9a3c599e
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-23 23:27:11 +0100

    Dashboard controller for bookings, my flats, simple view

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,14 @@
+class DashboardController < ApplicationController
+  def all_bookings
+    @bookings = current_user.bookings
+  end
+
+  def all_flats
+    @flats = current_user.flats
+  end
+end
+
+
+# 1. show all my flats - created
+# 2. show all my bookings - others owners
+# User.order('id ASC').reorder('name DESC')
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  # get 'dashboard/all_bookings'
+  # get 'dashboard/all_flats'
+
   devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:edit, :update]
-  resources :flats do
-    resources :bookings, only: [ :index, :new, :create ]
+  resources :users, only: [ :edit, :update ]
 
+  resources :flats do
+    resources :bookings, only: [ :new, :create ]
   end
+  # dashboard/bookings or dashboard/flats
+  get '/dashboard/bookings', to: 'dashboard#all_bookings', as: 'bookings'
+  get '/dashboard/flats', to: 'dashboard#all_flats', as: 'all_flats'
 end
