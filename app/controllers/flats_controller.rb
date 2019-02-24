3337503925572a5ff9b59a715b4ba0f89a37ef21
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-24 20:56:48 +0100

    Pundit for flat - listings, add new, show, edit, update NEXT bookings

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,18 +3,22 @@ class FlatsController < ApplicationController
 
   def index
     # will change by pundit
-    @flats = Flat.all
+    @flats = policy_scope(Flat).order(created_at: :desc)
+    # @flats = Flat.all
   end
 
   # render form
   def new
     @flat = Flat.new
+    authorize @flat
   end
 
   def create
     @flat = Flat.new(flat_params)
     # assign signed-in user
     @flat.user = current_user
+    authorize @flat
+
     if @flat.save
       redirect_to flat_path(@flat)
     else
@@ -23,14 +27,17 @@ class FlatsController < ApplicationController
   end
 
   def show
+    # authorization in set_flat
   end
 
   def edit
+    authorize @flat
   end
 
   def update
     # allowed params; redirect to concrete flat
     if @flat.update(flat_params)
+      authorize @flat
       redirect_to @flat
     else
       render :edit
@@ -44,6 +51,7 @@ class FlatsController < ApplicationController
 
   def set_flat
     @flat = Flat.find(params[:id])
+    authorize @flat
   end
 
   def flat_params
---
+++ app/policies/flat_policy.rb
@@ -0,0 +1,35 @@
+class FlatPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  # def new?
+  #   return true
+  # end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  # need to be also the owner -> not just signin
+  def edit?
+    # true
+    user_is_owner?
+  end
+
+  def update?
+    # true
+    user_is_owner?
+  end
+
+  def user_is_owner?
+    # i do not use in pundit current_user BUT user
+    record.user == user
+  end
+end
