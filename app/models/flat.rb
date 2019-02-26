56f7653680e2922ff3b14799a3d94874a3789ee2
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-26 23:23:16 +0100

    Gemfile - CarrierWafe for photo uploader

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,9 +2,12 @@ class Flat < ApplicationRecord
   belongs_to :user
 
   has_many :bookings, dependent: :destroy
+
+  mount_uploader :photo, PhotoUploader
   validates :name, :description, :photo, :price, :address, :location, :capacity, presence: true
 end
 
 
 # 1. rails g model Flat name:string description:text photo:string price:integer address:string location:string capacity:integer  user:references
 # 2. has many bookings -> when destroy flat -> bookings as well
+# 3. added carrierwave gem for photo - app/uploaders/photo_uploader.rb, form update for flat
